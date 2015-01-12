df0218c2337d0694a34e9ebd1755baa64c2643a0
ZedsTed <zedsted@gmail.com>
2015-01-12 15:26:15 +0000

    Added in no arg constructor. Refactored the gethash functions to not scan the whole dir and just perform the operations on a single file.

--- Steam Web Artefact Reader/HashReader.cs
+++ Steam Web Artefact Reader/HashReader.cs
@@ -14,6 +14,9 @@ namespace Steam_Web_Artefact_Reader
         bool md5, sha1 = false;
         byte[] md5hash, sha1hash;
 
+        public HashReader()
+        { }
+
         /// <summary>
         /// 
         /// </summary>
@@ -40,36 +43,34 @@ namespace Steam_Web_Artefact_Reader
         /// 
         /// </summary>
         /// <returns></returns>
-        public string GetMD5Hash()
+        public string MD5Hash()
         {
             // Create a DirectoryInfo object representing the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(filePath);
+            //DirectoryInfo dir = new DirectoryInfo(filePath);
             // Get the FileInfo objects for every file in the directory.
-            FileInfo[] files = dir.GetFiles();
+            FileInfo files = new FileInfo(filePath);
 
             MD5 md5HashObj = MD5.Create();
-            
-            foreach (FileInfo fInfo in files)
-            {
-                // Create a fileStream for the file.
-                FileStream fileStream = fInfo.Open(FileMode.Open);
-                // Be sure it's positioned to the beginning of the stream.
-                fileStream.Position = 0;
-                // Compute the hash of the fileStream.
-                md5hash = md5HashObj.ComputeHash(fileStream);
-                StringBuilder sBuilder = new StringBuilder();
-                // Loop through each byte of the hashed data  
-                // and format each one as a hexadecimal string. 
-                for (int i = 0; i < md5hash.Length; i++)
-                {
-                    sBuilder.Append(md5hash[i].ToString("x2"));
-                }
 
-                md5HashString = Convert.ToString(sBuilder);
-                
-                // Close the file.
-                fileStream.Close();
+            // Create a fileStream for the file.
+            FileStream fileStream = files.Open(FileMode.Open);
+            // Be sure it's positioned to the beginning of the stream.
+            fileStream.Position = 0;
+            // Compute the hash of the fileStream.
+            md5hash = md5HashObj.ComputeHash(fileStream);
+            StringBuilder sBuilder = new StringBuilder();
+            // Loop through each byte of the hashed data  
+            // and format each one as a hexadecimal string. 
+            for (int i = 0; i < md5hash.Length; i++)
+            {
+                sBuilder.Append(md5hash[i].ToString("x2"));
             }
+
+            md5HashString = Convert.ToString(sBuilder);
+
+            // Close the file.
+            fileStream.Close();            
+            
 
             return md5HashString;
         }
@@ -78,37 +79,36 @@ namespace Steam_Web_Artefact_Reader
         /// 
         /// </summary>
         /// <returns></returns>
-        public string GetSHA1Hash()
+        public string SHA1Hash()
         {
             // Create a DirectoryInfo object representing the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(filePath);
+            //DirectoryInfo dir = new DirectoryInfo(filePath);
             // Get the FileInfo objects for every file in the directory.
-            FileInfo[] files = dir.GetFiles();
+            FileInfo files = new FileInfo(filePath);
 
             SHA1 sha1HashObj = SHA1.Create();
             //byte[] md5hash;
 
-            foreach (FileInfo fInfo in files)
+            // Create a fileStream for the file.
+            FileStream fileStream = files.Open(FileMode.Open);
+            // Be sure it's positioned to the beginning of the stream.
+            fileStream.Position = 0;
+            // Compute the hash of the fileStream.
+            sha1hash = sha1HashObj.ComputeHash(fileStream);
+
+            StringBuilder sBuilder = new StringBuilder();
+            // Loop through each byte of the hashed data  
+            // and format each one as a hexadecimal string. 
+            for (int i = 0; i < sha1hash.Length; i++)
             {
-                // Create a fileStream for the file.
-                FileStream fileStream = fInfo.Open(FileMode.Open);
-                // Be sure it's positioned to the beginning of the stream.
-                fileStream.Position = 0;
-                // Compute the hash of the fileStream.
-                sha1hash = sha1HashObj.ComputeHash(fileStream);
-
-                StringBuilder sBuilder = new StringBuilder();
-                // Loop through each byte of the hashed data  
-                // and format each one as a hexadecimal string. 
-                for (int i = 0; i < sha1hash.Length; i++)
-                {
-                    sBuilder.Append(sha1hash[i].ToString("x2"));
-                }
-
-                sha1HashString = Convert.ToString(sBuilder);
-                // Close the file.
-                fileStream.Close();
+                sBuilder.Append(sha1hash[i].ToString("x2"));
             }
+
+            sha1HashString = Convert.ToString(sBuilder);
+            // Close the file.
+            fileStream.Close();
+
+            
             return sha1HashString;        
         }
 
