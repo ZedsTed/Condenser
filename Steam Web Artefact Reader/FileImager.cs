99e20dff7614b1ad72b8c409933b46148379c211
ZedsTed <zedsted@gmail.com>
2015-01-12 15:24:09 +0000

    Preliminary class to copy/image the files.

---
+++ Steam Web Artefact Reader/FileImager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam_Web_Artefact_Reader
+{
+    class FileImager
+    {
+        public FileImager()
+        { }
+
+        public FileImager(string dir)
+        { }
+
+
+    }
+}
